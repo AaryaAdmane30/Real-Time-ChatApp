dc05acab0b8fa3b3bbfdd6196c52a0bbc4cb564b
AaryaAdmane30 <admaneaarya@gmail.com>
2025-04-02 01:31:59 +0530

    lol

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,18 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
-import dotenv from "dotenv"; // for the Env fi;en
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import dotenv from "dotenv"; // for the Env fi;en
 dotenv.config();
 
 // const app = express(); // already declared in socket.js
 
-const Port = process.env.PORT || 5001; // reading the env file
+const PORT = process.env.PORT || 5001; // reading the env file
 const __dirname = path.resolve(); // for deployment
-
-
 
 // Middleware (CORS is at the top)
 app.use(express.json({ limit: "50mb" })); // Increase payload size so that we can upload our profile photo largery
@@ -42,6 +40,6 @@ if (process.env.NODE_ENV === "production") {
 
 // app.listen(PORT, () => { // we will be usig socket server for real time communication between client and server
 server.listen(PORT, () => {
-  console.log(`Server is Running on PORT ${Port} successfully`);
+  console.log(`Server is Running on PORT ${PORT} successfully`);
   connectDB(); // from db.js to connect to mongo atlas
 });
